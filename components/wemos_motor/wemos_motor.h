96a6e009e05fc7a60d22cfb5eca37a9a4a11c3d7
abdoshamma <abdoshamma7371@gmail.com>
2024-11-01 13:55:37 +0300

    Update wemos_motor.h

--- components/wemos_motor/wemos_motor.h
+++ components/wemos_motor/wemos_motor.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
+#include "esphome/components/output/float_output.h"  // Add this include
 #include "Wire.h"
 
 namespace esphome {
@@ -16,16 +17,31 @@ namespace wemos_motor {
 #define _STANDBY 4
 
 class WemosMotor : public Component {
- public:
+public:
   WemosMotor(uint8_t address, uint8_t motor, uint32_t freq);
   void setup() override;
   void setfreq(uint32_t freq);
   void setmotor(uint8_t dir, float pwm_val);
 
- protected:
+protected:
   uint8_t _address;
   uint8_t _motor;
   uint32_t _freq;
+};
+
+// Add the output class here
+class WemosMotorOutput : public output::FloatOutput {
+ public:
+  void set_motor(WemosMotor *motor) { motor_ = motor; }
+  void write_state(float state) override {
+    if (state > 0) {
+      motor_->setmotor(_CW, state * 100);
+    } else {
+      motor_->setmotor(_STOP, 0);
+    }
+  }
+ protected:
+  WemosMotor *motor_;
 };
 
 }  // namespace wemos_motor
